8206327b8fbeae623a0e6380029e83f7ab9bb201
Denis Bollinne <dbo@opinum.com>
2016-05-12 11:51:15 +0200

    Allows to skip appending to redis list for X seconds when connection can't be opened

--- src/redis4net/Appender/RedisAppender.cs
+++ src/redis4net/Appender/RedisAppender.cs
@@ -1,42 +1,108 @@
 ﻿using System;
 using log4net.Appender;
 using log4net.Core;
+using log4net.Util;
 
 namespace redis4net.Appender
 {
-	using redis4net.Redis;
+    using redis4net.Redis;
 
-	public class RedisAppender : AppenderSkeleton
-	{
-		protected ConnectionFactory ConnectionFactory { get; set; }
-		public string RemoteAddress { get; set; }
-		public int RemotePort { get; set; }
-		public string ListName { get; set; }
+    public class RedisAppender : AppenderSkeleton
+    {
+        private DateTime? errorStateDateTime;
+        protected ConnectionFactory ConnectionFactory { get; set; }
+        public string RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+        public string ListName { get; set; }
 
-		public override void ActivateOptions()
-		{
-			base.ActivateOptions();
+        public int SecondsBeforeOpeningConnectionAfterFailed { get; set; } = 0;
+        public int FailedConnectionRetryTimeoutInSeconds { get; set; } = 1;
 
-			InitializeConnectionFactory();
-		}
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+            this.ErrorHandler = new LogAllErrorHandler(new OnlyOnceErrorHandler());
 
-		protected virtual void InitializeConnectionFactory()
-		{
-			var connection = new Connection();
-			ConnectionFactory = new ConnectionFactory(connection, RemoteAddress, RemotePort, 1, ListName);
-		}
+            InitializeConnectionFactory();
 
-		protected override void Append(log4net.Core.LoggingEvent loggingEvent)
-		{
-			try
-			{
-				var message = RenderLoggingEvent(loggingEvent);
-				ConnectionFactory.GetConnection().AddToList(message);
-			}
-			catch(Exception exception)
-			{
-				ErrorHandler.Error("Unable to send logging event to remote redis host " + RemoteAddress + " on port " + RemotePort, exception, ErrorCode.WriteFailure);
-			}
-		}
-	}
+            ErrorHandler.Error("!!!!!!Calling ActivateOptions!!!!!");
+        }
+
+
+        protected virtual void InitializeConnectionFactory()
+        {
+            var connection = new Connection();
+            ConnectionFactory = new ConnectionFactory(connection, RemoteAddress, RemotePort, FailedConnectionRetryTimeoutInSeconds, ListName);
+        }
+
+        protected override void Append(log4net.Core.LoggingEvent loggingEvent)
+        {
+            if (errorStateDateTime.HasValue)
+            {
+                if (errorStateDateTime.Value.AddSeconds(SecondsBeforeOpeningConnectionAfterFailed) < DateTime.UtcNow)
+                {
+                    errorStateDateTime = null;
+                    AppendCore(loggingEvent);
+                }
+                else
+                {
+                    ErrorHandler.Error("Unable to send logging event to remote redis host " + RemoteAddress + " on port " + RemotePort + " because the connection couldnt be opened");
+                }
+            }
+            else
+            {
+                AppendCore(loggingEvent);
+            }
+        }
+
+        private void AppendCore(LoggingEvent loggingEvent)
+        {
+            try
+            {
+                var message = RenderLoggingEvent(loggingEvent);
+                var connection = ConnectionFactory.GetConnection();
+                if (connection != null)
+                {
+                    connection.AddToList(message);
+                }
+                else
+                {
+                    this.errorStateDateTime = DateTime.UtcNow;
+                }
+            }
+            catch (Exception exception)
+            {
+                ErrorHandler.Error("Unable to send logging event to remote redis host " + RemoteAddress + " on port " + RemotePort, exception, ErrorCode.WriteFailure);
+                this.errorStateDateTime = DateTime.UtcNow;
+            }
+        }
+    }
+
+    public class LogAllErrorHandler : IErrorHandler
+    {
+        private readonly OnlyOnceErrorHandler onlyOnceErrorHandler;
+
+        public LogAllErrorHandler(OnlyOnceErrorHandler onlyOnceErrorHandler)
+        {
+            this.onlyOnceErrorHandler = onlyOnceErrorHandler;
+        }
+
+        public void Error(string message, Exception e, ErrorCode errorCode)
+        {
+            onlyOnceErrorHandler.Error(message, e, errorCode);
+            onlyOnceErrorHandler.Reset();
+        }
+
+        public void Error(string message, Exception e)
+        {
+            onlyOnceErrorHandler.Error(message, e);
+            onlyOnceErrorHandler.Reset();
+        }
+
+        public void Error(string message)
+        {
+            onlyOnceErrorHandler.Error(message);
+            onlyOnceErrorHandler.Reset();
+        }
+    }
 }
